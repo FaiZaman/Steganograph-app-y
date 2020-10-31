b4ac3f10351b33c0885009df146b37c35b415c38
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-10-31 17:02:25 +0000

    Added navigation between the GUI screens

--- Main.py
+++ Main.py
@@ -1,46 +1,110 @@
+import sys
 import PySimpleGUI as gui
 
 class GraphicalUserInterface(object):
 
     def __init__(self):
+        
+        self.app_name = "Steganograph-App-y"
 
-        self.home_screen = [
-            [gui.Text('Welcome to Steganograph-app-y!', font=('Helvetica', 16), justification='center')],
+
+    def create_home_window(self):
+
+        home_screen = [
+            [gui.Text('Welcome to {0}!'.format(self.app_name), font=('Helvetica', 16), 
+                justification='center')],
             [gui.Text('_'  * 100, size=(65, 1))],
             [gui.Text('Please select whether you would like to embed or extract a secret message.', 
                 font=('Helvetica', 12))],
-            [gui.Button('Embed'), gui.Button('Extract'), gui.Cancel()]
+            [gui.Button('Embed'), gui.Button('Extract'), gui.Button('Exit')]
         ]
 
-        self.embed_screen = [
+        home_window = gui.Window('{}'.format(self.app_name), home_screen)
+        return home_window
+
+
+    def create_embedding_window(self):
+
+        embedding_screen = [
             [gui.Text('Embedding', font=('Helvetica', 15), justification='center')],
             [gui.Text('Choose File: ')],
-            [gui.Text('Secret Key: ')]
+            [gui.Text('Secret Key: ')],
+            [gui.Button('Back to Main Menu')]
         ]
 
-        self.extract_screen = [
+        embed_window = gui.Window('{0} - Embedding'.format(self.app_name), embedding_screen)
+        return embed_window
+
+
+    def create_extracting_window(self):
+        
+        extracting_screen = [
             [gui.Text('Extracting', font=('Helvetica', 15), justification='center')],
             [gui.Text('Choose File: ')],
-            [gui.Text('Secret Key')]
+            [gui.Text('Secret Key')],
+            [gui.Button('Back to Main Menu')]
         ]
-    
+
+        extract_window = gui.Window('{0} - Extracting'.format(self.app_name), extracting_screen)
+        return extract_window
+
+
     def display(self):
 
-        window = gui.Window('App', self.home_screen)
-        embedding = extracting = False
+        selecting = True
 
-        while True:
-            event, values = window.read()
-            if event in (None, 'Cancel'):
-                break
-            if event == 'Embed':
-                embedding = True
-            if event == 'Extract':
-                extracting = True
+        while selecting:
+
+            window = self.create_home_window()
+            embedding = extracting = False
+
+            while True:
+                event, values = window.read()
+                if event in (None, 'Exit'):
+                    sys.exit()
+                    break
+                if event == 'Embed':
+                    embedding = True
+                    break
+                if event == 'Extract':
+                    extracting = True
+                    break
+
+            window.close()
+
+            if embedding:
+
+                window = self.create_embedding_window()
+
+                while True:
+                    event, values = window.read()
+                    if event is None:
+                        sys.exit()
+                    if event == 'Choose File':
+                        pass
+                    if event == 'Back to Main Menu':
+                        break
+
+                window.close()
+
+
+            if extracting:
+
+                window = self.create_extracting_window()
+
+                while True:
+                    event, values = window.read()
+                    if event is None:
+                        sys.exit()
+                    if event == 'Choose File':
+                        pass
+                    if event == 'Back to Main Menu':
+                        break
+
+                window.close()
 
 
 if __name__ == '__main__':
 
     GUI = GraphicalUserInterface()
     GUI.display()
-
