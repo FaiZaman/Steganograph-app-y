d28850495e6da15934a9b0eff86bfa1f488a7874
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-11-07 18:08:38 +0000

    Finished encoding data into image with agnostic colour channels

--- algorithms/LSB.py
+++ algorithms/LSB.py
@@ -12,18 +12,21 @@ class LSB():
         self.key = key
 
 
-    def embed_pixel(self, pixel, colour, message_index, message_length, bit):
+    def embed_pixel(self, pixel, colour, colour_index, message_index, message_length):
 
+        # embed a bit of the data into the Least Significant Bit of the cover image's current pixel
         if message_index < message_length:
-            pixel[0] = int(colour[:-1] + message_bit, 2)    # make agnostic
+            bit = self.message[message_index]
+            pixel[colour_index] = int(colour[:-1] + bit, 2)
         else:
-            return None
+            return np.array([])     # no more data to embed
 
         return pixel
 
 
     def encode(self):
 
+        # get dimensions of cover image
         width = np.size(self.cover, 1)
         height = np.size(self.cover, 0)
         num_bytes = width * height * 3   # 3 colour channels
@@ -35,22 +38,31 @@ class LSB():
         if message_length > num_bytes:
             raise ValueError("The message is too large for the image.")
 
+        cover_image = self.cover    # so cover is not modified
+
         # loop through image pixels
         for x in range(0, width):
             for y in range(0, height):
 
-                pixel = self.cover[y][x]
+                # assign, retrieve, and convert RGB values
+                pixel = cover_image[y][x]
                 embedded_pixel = pixel
-
                 r, g, b = message_to_binary(pixel)
-                message_bit = self.message[message_index]
 
-                for colour in [r, g, b]:
-                    embedded_pixel = self.embed_pixel\
-                        (embedded_pixel, colour, message_index, message_length, message_bit)
-                    #if embedded_pixel:
-                     #   self.cover[y][x] = 
-                    message_index += 1
+                # embed message data in each colour channel
+                for colour_index, colour in enumerate([r, g, b]):
 
-                break
-            break
+                    # embed data within current pixel
+                    embedded_pixel = self.embed_pixel(embedded_pixel, colour,\
+                        colour_index, message_index, message_length)
+
+                    # reassign embedded pixel to cover image
+                    if embedded_pixel.any():
+                        cover_image[y][x] = embedded_pixel
+                        message_index += 1
+                    else:
+                        break   # no more data so break
+
+        # reassign and return stego image
+        self.stego = cover_image
+        return self.stego
