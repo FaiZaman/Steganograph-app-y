3a516a02332aba6ae47dd6dd6e0018d2dd06246c
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-12-05 13:38:40 +0000

    Extracted original message but does not match original - debug time

--- algorithms/LSBMR.py
+++ algorithms/LSBMR.py
@@ -111,4 +111,15 @@ class LSBMR(LSBM, PVD):
             next_coordinates, stego_block = self.get_pixel_block(x, y)
             first_stego_pixel, second_stego_pixel = stego_block[0], stego_block[1]
 
-            
+            # extract both bits from the pixel pair
+            first_binary_pixel = integer_to_binary(first_stego_pixel)
+            first_msg_bit = first_binary_pixel[-1]
+            second_msg_bit = self.binary_function(first_stego_pixel, second_stego_pixel)
+
+            binary_message += first_msg_bit + second_msg_bit
+        
+        # extract the original message, save to file, and return
+        extracted_message = binary_to_string(binary_message, self.delimiter)
+        save_message(self.save_path, self.time_string, extracted_message)
+
+        return extracted_message
