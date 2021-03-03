ab26b5f1607f932ae9b480bcfa17055fdec570ed
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-03-03 11:40:42 +0000

    Embedded message into edge pixel coordinates using LSBMR embedding

--- algorithms/Hybrid_LSBMR.py
+++ algorithms/Hybrid_LSBMR.py
@@ -1,4 +1,6 @@
+import random
 from algorithms.LSBMR import LSBMR
+from utility import message_to_binary, save_image
 
 class Hybrid_LSBMR(LSBMR):
 
@@ -27,4 +29,47 @@ class Hybrid_LSBMR(LSBMR):
     # generates pixel path through edge coordinates
     def embed_image(self):
 
+        self.message = message_to_binary(self.message)
+        message_index = 0
+        message_length = len(self.message)
+
         edge_coordinates = self.get_edge_coordinates()
+        num_edge_coordinates = len(edge_coordinates)
+
+        if message_length > num_edge_coordinates:
+            raise ValueError("The message is too large for the image.")
+
+        # get a random path based on seed through the edge pixels
+        path = random.sample(edge_coordinates, num_edge_coordinates)
+        cover_image = self.image  # so image is not modified
+
+        for (y, x) in path:
+
+            # compute the two-pixel block and the coordinates of the next pixel
+            next_coordinates, block = self.get_pixel_block(x, y)
+
+            # assigning
+            first_pixel, second_pixel = block[0], block[1]
+            next_x, next_y = next_coordinates[0], next_coordinates[1]
+
+            # check if not 0 or 255 as embedding cannot be performed otherwise
+            if 0 < first_pixel < 255 and 0 < second_pixel < 255:
+
+                # use LSBMR embedding and output stego pixels
+                first_stego_pixel, second_stego_pixel =\
+                    self.embed_pixels(first_pixel, second_pixel, message_index)
+
+                # reassign new stego pixels and increment message index
+                cover_image[y][x] = first_stego_pixel
+                cover_image[next_y][next_x] = second_stego_pixel
+
+                message_index += 2
+
+                if message_index == message_length:
+                    break
+
+        # reassign, save, and return stego image
+        stego_image = cover_image
+        save_image(self.save_path, self.image_name, self.time_string, stego_image)
+
+        return stego_image
