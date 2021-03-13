76117a0ffb5b831630ce1b554d1855bded976d2d
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-03-13 14:06:46 +0000

    Implemented PSNR and started mass generation

--- evaluation/MSE.py
+++ evaluation/MSE.py
@@ -44,15 +44,3 @@ class MSE():
 
         mean_squared_error = self.calculate_image_error(cover_image, stego_image)
         return mean_squared_error
-
-"""
-MSE = MSE()
-cover_path = 'C:/Users/faizz/University Work/Year 4/Advanced Project/Images/Cover/Lena.png'
-stego_path = 'C:/Users/faizz/University Work/Year 4/Advanced Project/Images/Stego/2021_03_09_13;55_Lena.png'
-
-cover = cv2.imread(cover_path, cv2.IMREAD_GRAYSCALE)
-stego = cv2.imread(stego_path, cv2.IMREAD_GRAYSCALE)
-
-error = MSE.get_error(cover, stego)
-print(error)
-"""
--- evaluation/PSNR.py
+++ evaluation/PSNR.py
@@ -2,17 +2,19 @@ import cv2
 import math
 from MSE import MSE
 
-class RMSE(MSE):
+class PSNR(MSE):
 
     def __init__(self):
 
         super().__init__()
-        self.name = "Root Mean Squared Error"
+        self.name = "Peak Signal to Noise Ratio"
 
 
-    # calculates MSE and takes square root
     def get_error(self, cover_image, stego_image):
 
         mean_squared_error = self.calculate_image_error(cover_image, stego_image)
-        root_mean_squared_error = math.sqrt(mean_squared_error)
-        return root_mean_squared_error
+
+        fractal = (255 * 255) / mean_squared_error
+        peak_signal_to_noise_ratio = 10 * math.log10(fractal)
+
+        return peak_signal_to_noise_ratio
