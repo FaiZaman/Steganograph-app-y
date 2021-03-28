64fe96744a4cd8f7f361ac8beca9ab38d115a655
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-03-28 11:45:22 +0100

    Final pushes to combine aletheia changes and commit

--- evaluation/mass_evaluation.py
+++ evaluation/mass_evaluation.py
@@ -10,35 +10,43 @@ extension = ".pgm"
 # command to run ensemble classifier:
 # python aletheia.py e4s-predict models/e4s_srm_bossbase_lsbm0.10_gs.model srm sample_images/1_lsb.pgm
 
-# initalise error metrics and parameters
-MSE = MSE()
-PSNR = PSNR()
-total_mse = 0
-total_psnr = 0
-num_images = 10000
 
-# loop through image files
-for filename in range(1, 10001):
+def evaluate(folder):
 
-    filename_string = str(filename) + extension
+    # initalise error metrics and parameters
+    MSE = MSE()
+    PSNR = PSNR()
+    total_mse = 0
+    total_psnr = 0
+    num_images = 10000
 
-    # load the cover image file
-    cover_path = cover_dataset_path + filename_string
-    cover = cv2.imread(cover_path, cv2.IMREAD_GRAYSCALE)
+    stego_dataset_path = stego_dataset_path + folder
 
-    stego_path = stego_dataset_path + filename_string
-    stego = cv2.imread(stego_path, cv2.IMREAD_GRAYSCALE)
+    # loop through image files
+    for filename in range(1, 10001):
 
-    mean_squared_error = MSE.get_error(cover, stego)
-    psnr = PSNR.get_error(cover, stego, mean_squared_error)
+        filename_string = str(filename) + extension
 
-    total_mse += mean_squared_error
-    total_psnr += psnr
+        # load the cover image file
+        cover_path = cover_dataset_path + filename_string
+        cover = cv2.imread(cover_path, cv2.IMREAD_GRAYSCALE)
 
-    print(filename, mean_squared_error, psnr)
+        stego_path = stego_dataset_path + filename_string
+        stego = cv2.imread(stego_path, cv2.IMREAD_GRAYSCALE)
 
-average_mse = total_mse / num_images
-average_psnr = total_psnr / num_images
+        mean_squared_error = MSE.get_error(cover, stego)
+        psnr = PSNR.get_error(cover, stego, mean_squared_error)
 
-print(average_mse)
-print(average_psnr)
+        total_mse += mean_squared_error
+        total_psnr += psnr
+
+        print(filename, mean_squared_error, psnr)
+
+    average_mse = total_mse / num_images
+    average_psnr = total_psnr / num_images
+
+    print(average_mse)
+    print(average_psnr)
+
+
+evaluate('LSB')
