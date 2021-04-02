dd699074ac78713545c82d917e1a48539d3c92a8
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-04-02 15:42:28 +0100

    Added functions in mass generation for standalone edge-LSBMR algorithms - bug to fix

--- mass_generation.py
+++ mass_generation.py
@@ -1,8 +1,12 @@
 import cv2
+
 from algorithms.LSB import LSB
 from algorithms.LSBM import LSBM
 from algorithms.LSBMR import LSBMR
 from algorithms.PVD import PVD
+from algorithms.Hybrid_LSBMR import Hybrid_LSBMR
+
+from edge_detectors.Sobel import Sobel
 
 # initialise dataset strings
 dataset_path = "C:/Users/faizz/University Work/Year 4/Advanced Project/Dataset/BOSSbase (Cover)/"
@@ -19,12 +23,12 @@ message_file = open(message_file, "r", encoding='utf-8')
 message = message_file.read()
 message_file.close()
 
-# loop through image files
-
-def generate(algorithm, algorithm_string, save_path):
+# generates stego images for specified algorithm
+def generate_basic(algorithm, algorithm_string, save_path):
 
     save_path = save_path + algorithm_string + '/'
 
+    # loop through image files
     for filename in range(1, 10001):
 
         filename_string = str(filename) + extension
@@ -41,7 +45,33 @@ def generate(algorithm, algorithm_string, save_path):
         print(filename)
 
 
-def validate(algorithm, algorithm_string, save_path):
+# generates stego images for edge detector + LSBMR embedding
+def generate_standalone(detector, detector_string, save_path):
+
+    save_path = save_path + detector_string + '/'
+
+    # loop through image files
+    for filename in range(1, 10001):
+
+        filename_string = str(filename) + extension
+
+        # load the cover image file
+        cover_path = dataset_path + filename_string
+        cover_image = cv2.imread(cover_path, cv2.IMREAD_GRAYSCALE)
+        cover_data = (filename_string, cover_image)
+
+        # initialise detector and detect edges
+        edge_detector = detector()
+        edges = edge_detector.detect(cover_image)
+
+        # initialise algorithm and embed in edges
+        Hybrid_LSBMR_algorithm = Hybrid_LSBMR(cover_data, edges, message, key, save_path)
+        Hybrid_LSBMR_algorithm.embed_image()
+
+        print(filename)
+
+
+def validate_basic(algorithm, algorithm_string, save_path):
 
     message = ""
     save_path = save_path + algorithm_string + '/'
@@ -61,5 +91,32 @@ def validate(algorithm, algorithm_string, save_path):
 
         print(message)
 
-generate(PVD, 'PVD', save_path)
-#validate(LSB, 'LSB', save_path)
+
+def validate_standalone(detector, detector_string, save_path):
+
+    message = ""
+    save_path = save_path + detector_string + '/'
+
+    # loop through image files
+    for filename in range(1, 10001):
+
+        filename_string = str(filename) + extension
+
+        # load the cover image file
+        stego_path = save_path + filename_string
+        stego_image = cv2.imread(stego_path, cv2.IMREAD_GRAYSCALE)
+        stego_data = (filename_string, stego_image)
+
+        # initialise detector and detect edges
+        edge_detector = detector()
+        edges = edge_detector.detect(stego_image)
+
+        # initialise algorithm and extract from edges
+        Hybrid_LSBMR_algorithm = Hybrid_LSBMR(stego_data, edges, message, key, save_path)
+        message = Hybrid_LSBMR_algorithm.extract()
+
+        print(message)
+
+
+#generate_standalone(Sobel, 'Sobel', save_path)
+validate_standalone(Sobel, 'Sobel', save_path)
